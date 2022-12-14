eb0af2fd1df479c83df1c46c1301b23e24e81f2e
Siddhant Raj <siddhantraj04@gmail.com>
2022-12-14 23:18:31 +0530

    Added Modal Component

--- Frontend/src/places/pages/UserPlaces.js
+++ Frontend/src/places/pages/UserPlaces.js
@@ -1,26 +1,41 @@
 import React from "react";
 
-import {useParams} from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
-const PLACES=[
-    {id:'p1',title:'Taj Mahal', description:'Seventh Wonder of The World', imgUrl:'https://lh5.googleusercontent.com/p/AF1QipNjQ5SJFQ43S6nQHYkSflVjCaHqQGQdS8eEIL1f=w408-h277-k-no',address :'Agra',
-    location:{
-        lat:27.1751825,lng:78.0397837
+const PLACES = [
+  {
+    id: "p1",
+    title: "Taj Mahal",
+    description: "Seventh Wonder of The World",
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNjQ5SJFQ43S6nQHYkSflVjCaHqQGQdS8eEIL1f=w408-h277-k-no",
+    address: "Agra",
+    location: {
+      lat: 27.1751825,
+      lng: 78.0397837,
     },
-    creator:'u1'},
-    {id:'p2',title:'Golden Gate Bridge', description:'A giant beautiful bridge', imgUrl:'https://lh5.googleusercontent.com/p/AF1QipN0-mJ4M1ftzod1vtrdwMyE2fmmqxGdPxnvQMH4=w408-h306-k-no',address :'San Francisco',
-    location:{
-        lat:27.1751825,lng:78.0397837
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Golden Gate Bridge",
+    description: "A giant beautiful bridge",
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipN0-mJ4M1ftzod1vtrdwMyE2fmmqxGdPxnvQMH4=w408-h306-k-no",
+    address: "San Francisco",
+    location: {
+      lat: 27.1751825,
+      lng: 78.0397837,
     },
-    creator:'u2'}
+    creator: "u2",
+  },
 ];
-const UserPlaces=()=>{
-    const userId=useParams().userId;
-    const loadedPlaces=PLACES.filter(place=>place.creator===userId);
-    return(
-        
-        <PlaceList items={loadedPlaces}/>
-        // <h1>Working</h1>
-    );  
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = PLACES.filter((place) => place.creator === userId);
+  return (
+    <PlaceList items={loadedPlaces} />
+    // <h1>Working</h1>
+  );
 };
 export default UserPlaces;
